0aff0955f8e44e186212114b293adef1f3c30a18
Sam <sammymenaged@yahoo.com>
2020-10-19 00:04:48 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -12,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-sam:Test123@cluster0-oql7h.mongodb.net/todolistDB",
+mongoose.connect("mongodb+srv://" + process.env.DATABASE_NAME + ":" + process.env.DATABASE_PASSWORD + "@cluster0-oql7h.mongodb.net/todolistDB",
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
