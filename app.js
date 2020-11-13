f7579dc83221f79fba4c34b8ed84523ae0f1cdc6
Sam <sammymenaged@yahoo.com>
2020-11-13 16:49:39 -0500

    testing authetication for deletion

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const e = require('express');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
 //Database connection
-mongoose.connect("mongodb+srv://" + process.env.DATABASE_NAME + ":" + process.env.DATABASE_PASSWORD + "@cluster0-oql7h.mongodb.net/todolistDB", {
+mongoose.connect("mongodb+srv://" + process.env.DATABASE_NAME + ":" + process.env.DATABASE_PASSWORD + "@cluster0-oql7h.mongodb.net/todolistDB",
+{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
@@ -27,10 +27,7 @@ mongoose.set('useFindAndModify', false);
 //items in a todo list
 const itemsSchema = {
   name: String,
-  completed: {
-    type: Number,
-    default: 0
-  },
+  completed: {type: Number, default: 0},
   time: Number,
   date: String,
   dateFinished: Number,
@@ -48,29 +45,22 @@ const listSchema = {
 const List = mongoose.model("List", listSchema);
 
 //home page
-app.get("/", function (req, res) {
-
-  Item.find({}, function (err, foundItems) {
-    res.render("list", {
-      listTitle: "Today",
-      newListItems: foundItems
-    });
+app.get("/", function(req, res) {
+  Item.find({}, function(err, foundItems){
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
   });
 });
 
 //custom pages
-app.get("/:customListName", function (req, res) {
+app.get("/:customListName", function(req, res) {
 
   //capatalize name of list
   const customListName = _.capitalize(req.params.customListName);
 
-
   //find list in database
-  List.findOne({
-    name: customListName
-  }, function (err, foundList) {
-    if (!err) {
-      if (!foundList) {
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err){
+      if (!foundList){
         //Create a new list
         const list = new List({
           name: customListName,
@@ -78,29 +68,24 @@ app.get("/:customListName", function (req, res) {
         list.save();
         res.redirect("/" + customListName);
       } else {
-
-
         //Show an existing list
-        res.render("list", {
-          listTitle: foundList.name,
-          newListItems: foundList.items
-        });
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
       }
     }
   });
 });
 
 //add button pressed
-app.post("/", function (req, res) {
+app.post("/", function(req, res) {
   //posts items
-  const itemName = req.body.newItem; //name of item
-  const listName = req.body.list; //name of page
+  const itemName = req.body.newItem;//name of item
+  const listName = req.body.list;//name of page
   //keep track of date and time for item
   var time = new Date().getTime();
-  var date = new Date(time).toString().substring(4, 15);
+  var date = new Date(time).toString().substring(4,15);
 
   //create new item
-  const item = new Item({
+  const item = new Item ({
     name: itemName,
     completed: 0,
     time: time,
@@ -115,9 +100,7 @@ app.post("/", function (req, res) {
     res.redirect("/");
   } else {
     //create new custom list or go to existing one
-    List.findOne({
-      name: listName
-    }, function (err, foundList) {
+    List.findOne({name: listName}, function(err, foundList) {
       foundList.items.push(item);
       foundList.save();
       res.redirect("/" + listName);
@@ -126,15 +109,16 @@ app.post("/", function (req, res) {
 });
 
 //if user creates new list
-app.post("/list", function (req, res) {
+app.post("/list", function(req, res) {
   const newList = req.body.newList;
   res.redirect("/" + newList);
 });
 
 //if delete or checkbox is pressed
-app.post("/delete", function (req, res) {
+app.post("/delete", function(req, res) {
 
   //find ID of items that user interacts with
+  const ip = req.ip;
   const deleteId = req.body.trash;
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
@@ -148,11 +132,11 @@ app.post("/delete", function (req, res) {
   if (listName === "Today") {
 
     //find the item that is checked
-    Item.findById(checkedItemId, function (err, item) {
+    Item.findById(checkedItemId, function(err, item){
       if (!err) {
         if (item != null) {
           //if the item is not completed yet make it completed
-          if (item.completed === 0) {
+          if(item.completed === 0) {
             item.completed++;
           }
           //otherwise remove completion status
@@ -193,38 +177,38 @@ app.post("/delete", function (req, res) {
         }
       }
     });
+    if (ip === "::1") {
+      //if delete button is pressed find it and delete it
+      Item.findByIdAndRemove(deleteId, function(err, foundItem) {
+        if(!err) {
+          res.redirect("/");
+        }
+      });
+    } else {
+      res.redirect("/");
+    }
 
-    //if delete button is pressed find it and delete it
-    Item.findByIdAndRemove(deleteId, function (err, foundItem) {
-      if (!err) {
-        res.redirect("/");
-      }
-    });
-
-    //if the user checks or deletes an item in a custom list
+  //if the user checks or deletes an item in a custom list
   } else {
 
     //find the custom list in the database
-    List.findOne({
-      name: listName
-    }, function (err, foundList) {
-      if (!err) {
-
+    List.findOne({name: listName}, function(err, foundList){
+      if (!err){
         //for each list update completion status
-        for (var i = 0; i < foundList.items.length; i++) {
+        for (var i = 0; i < foundList.items.length; i++)
+        {
 
           //if the item matches the item that was checked
           if (foundList.items[i]._id == checkedItemId)
 
             //if not completed yet update to completed
-            if (foundList.items[i].completed === 0) {
+            if(foundList.items[i].completed === 0) {
               foundList.items[i].completed++;
             }
-
-          //otherwise remove completion status
-          else {
-            foundList.items[i].completed--;
-          }
+            //otherwise remove completion status
+            else {
+              foundList.items[i].completed--;
+            }
           if (foundList.items[i]._id == checkedItemId) {
 
             //find time elapsed from start to completion and convert to proper units
@@ -243,35 +227,27 @@ app.post("/delete", function (req, res) {
               elapsed /= 60;
               elapsedString = "minutes";
             }
-
             //update item in database
             elapsed = Math.round(elapsed);
             foundList.items[i].dateFinished = elapsed;
             foundList.items[i].units = elapsedString;
-          }
-        }
-        foundList.save();
-
-        //delete item from custom list
-        List.findOneAndUpdate({
-          name: listName
-        }, {
-          $pull: {
-            items: {
-              _id: deleteId
-            }
-          }
-        }, function (err, foundList) {
-          if (!err) {
-            res.redirect("/" + listName);
-          }
-        });
+      }
+    }
+    foundList.save();
+    //delete item from custom list
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: deleteId}}}, function(err, foundList){
+      if (!err){
+        res.redirect("/" + listName);
       }
     });
   }
+    });
+  }
+
+
 });
 
-app.get("/about", function (req, res) {
+app.get("/about", function(req, res){
   res.render("about");
 });
 
@@ -280,6 +256,6 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port, function () {
+app.listen(port, function() {
   console.log("Server Started...");
 });
